55a8a075b7e393323245d93f655eeafb745d34cb
Joaquin <luduenajoa3@gmail.com>
2022-07-02 13:49:51 -0300

    Componente informacion cancion

---
+++ src/components/BuscarPrincipal.vue
@@ -0,0 +1,9 @@
+<template>
+  
+</template>
+
+<script>
+export default {
+  name: 'BuscarPrincipal'
+}
+</script>
---
+++ src/components/Informacion.vue
@@ -0,0 +1,45 @@
+<template>
+  <div id="info">
+    <ul>
+      <li>
+        <h2>Titulo: {{canciones[0].title}}</h2>
+        <h2>Duracion: {{duracion}}min</h2>
+        <hr id = "duracion">
+        <h2>Autor: {{canciones[0]['artist-credit'][0].name}}</h2>
+      </li>
+    </ul>
+  </div>
+</template>
+
+<script>
+
+
+export default {
+  name:'informacionCanciones',
+  props:{
+    canciones:{
+      type: Array,
+      required: true,}
+  },
+  computed:{
+    duracion(){
+      let segundos=this.canciones[0].length;
+     
+      return ((segundos /1000)/60).toFixed(2);
+    }
+  }
+}
+</script>
+
+<style scoped>
+ul{
+list-style: none;
+}
+#duracion{
+  width: 30%;
+  margin: 0 35%;
+  height: 2px;
+  background-color: black;
+  border: solid black;
+}
+</style>
